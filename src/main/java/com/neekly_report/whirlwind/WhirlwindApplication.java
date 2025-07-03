7659e294fc74e8aa096fc782d6c583695ced5311
Jinnny-An <58163606+Jinnny-An@users.noreply.github.com>
2025-07-03 21:28:18 +0900

    First Commit

---
+++ src/main/java/com/neekly_report/whirlwind/WhirlwindApplication.java
@@ -0,0 +1,13 @@
+package com.neekly_report.whirlwind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhirlwindApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhirlwindApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neekly_report/whirlwind/WhirlwindApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neekly_report.whirlwind;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhirlwindApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
