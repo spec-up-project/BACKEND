bcdd52f65ee0bcdb8e4973c2e54b715320354f40
최윤정 <em220913@hankooktech.com>
2025-08-05 16:48:05 +0900

    [USER] api 설명 추가 - swagger표기

--- src/main/java/com/neekly_report/whirlwind/user/UserApiController.java
+++ src/main/java/com/neekly_report/whirlwind/user/UserApiController.java
@@ -1,5 +1,10 @@
 package com.neekly_report.whirlwind.user;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -17,22 +22,26 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserApiController {
     private final UserService userService;
 
+    @Operation(summary = "회원가입")
     @PostMapping("/register")
-    public UserDTO.Response.UserRegisterResponse createUser(@Validated @RequestBody UserDTO.Request.UserRegisterRequest dto) {
+    public UserDTO.Response.UserRegisterResponse register(@Validated @RequestBody UserDTO.Request.UserRegisterRequest dto) {
         return userService.register(dto);
     }
 
+    @Operation(summary = "로그인")
     @PostMapping("/login")
     public UserDTO.Response.LoginResponse login(@Validated @RequestBody UserDTO.Request.LoginRequest dto) {
         return userService.login(dto);
     }
 
+    @Operation(summary = "Access Token 재발급")
     @PostMapping("/reissue")
     public ResponseEntity<?> reissue(@RequestBody UserDTO.Request.TokenReissueRequest dto) {
         String newAccessToken = userService.reissueToken(dto.getRefreshToken());
         return ResponseEntity.ok(newAccessToken);
     }
 
+    @Operation(summary = "로그아웃")
     @PostMapping("/logout")
     public ResponseEntity<?> logout(Authentication authentication) {
         String userId = (String) authentication.getPrincipal();
