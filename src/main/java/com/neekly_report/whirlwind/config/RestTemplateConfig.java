c86dfa14bd2381ec54c1924c8fd9271ebaa949ea
최윤정 <em220913@hankooktech.com>
2025-08-07 16:53:26 +0900

    [LLM] duckling 세팅 수정

--- src/main/java/com/neekly_report/whirlwind/config/RestTemplateConfig.java
+++ src/main/java/com/neekly_report/whirlwind/config/RestTemplateConfig.java
@@ -3,13 +3,21 @@ package com.neekly_report.whirlwind.config;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class RestTemplateConfig {
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(ObjectMapper objectMapper) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Jackson 메시지 컨버터 명시적으로 설정
+        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setObjectMapper(objectMapper);
+        restTemplate.getMessageConverters().add(0, converter);
+
+        return restTemplate;
     }
 }
