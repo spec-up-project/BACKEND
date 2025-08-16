f555d9b62144f2ed98b35fb012f7821eb4b4f441
Yoon <0224cyj@gmail.com>
2025-08-16 20:52:30 +0900

    [webclient] import 제거

--- src/main/java/com/neekly_report/whirlwind/config/WebClientConfig.java
+++ src/main/java/com/neekly_report/whirlwind/config/WebClientConfig.java
@@ -1,7 +1,5 @@
 package com.neekly_report.whirlwind.config;
 
-import io.netty.handler.ssl.SslContextBuilder;
-import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
@@ -9,7 +7,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 import reactor.netty.http.client.HttpClient;
 import reactor.netty.resources.ConnectionProvider;
 
-import javax.net.ssl.SSLException;
 import java.time.Duration;
 
 @Configuration
